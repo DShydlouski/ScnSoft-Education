6846696218859d16831ee41ab481a884b6e4ed67
dshydlouski <dshydlouski@scnsoft.com>
2024-01-04 13:12:19 +0300

    #1: Fixing issues with code and submission resultts

--- ScnSoft-Education/LeetCodeTraining/TwoSum1/TwoSumSolution.cs
+++ ScnSoft-Education/LeetCodeTraining/TwoSum1/TwoSumSolution.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LeetCodeTraining.TwoSum1;
 
-namespace LeetCodeTraining.TwoSum1
+//https://leetcode.com/problems/two-sum/
+public static class TwoSumSolution
 {
-    //https://leetcode.com/problems/two-sum/
-    public static class TwoSumSolution
+    public static int[] TwoSum(int[] nums, int target)
     {
-        public static int[] TwoSum(int[] nums, int target)
+        if (nums == null || nums.Length < 2)
         {
-            if (nums == null || nums.Length < 2) return null;
-            for (int i = 0; i < nums.Length - 1; i++)
+            return null;
+        }
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
             {
-                for (int j = i + 1; j < nums.Length; j++)
+                if (nums[i] + nums[j] == target)
                 {
-                    if (nums[i] + nums[j] == target) return new int[] { i, j };
+                    return new int[] { i, j };
                 }
             }
-            return new int[] { };
         }
+        return new int[] { };
     }
 }
+
