18dc26cc5f8137884b4d9a07a35defbc69cf4ab4
dshydlouski <dshydlouski@scnsoft.com>
2024-01-05 13:17:05 +0300

    #9: Solution to a Palindrome Number problem with unit tests

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/PalindromeNumberSolitionTests.cs
@@ -0,0 +1,52 @@
+﻿using LeetCodeTraining.PalindromeNumber9;
+
+namespace LeetCodeTraining.Tests
+{
+    public class PalindromeNumberSolitionTests
+    {
+        [Fact]
+        public void PalindromeNumber_StringWith3Nums()
+        {
+            //Arrange
+            int x = 121;
+            bool expectedResult = true;
+            bool result;
+
+            //Act
+            result = PalidnromeNumberSolution.IsPalindrome(x);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void PalindromeNumber_StringWithNegativeNums()
+        {
+            //Arrange
+            int x = -121;
+            bool expectedResult = false;
+            bool result;
+
+            //Act
+            result = PalidnromeNumberSolution.IsPalindrome(x);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void PalindromeNumber_StringWith2Nums()
+        {
+            //Arrange
+            int x = 10;
+            bool expectedResult = false;
+            bool result;
+
+            //Act
+            result = PalidnromeNumberSolution.IsPalindrome(x);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/PalindromeNumber9/PalidnromeNumberSolution.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LeetCodeTraining.PalindromeNumber9;
+
+public static class PalidnromeNumberSolution
+{
+    public static bool IsPalindrome(int x)
+    {
+        char[] reversed = x.ToString().ToCharArray();
+        Array.Reverse(reversed);
+        return x.ToString() == new string(reversed);
+    }
+}
