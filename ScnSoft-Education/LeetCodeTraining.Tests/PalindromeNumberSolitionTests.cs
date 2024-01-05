0fe26f8e0efa018782928980ce775488ca61e024
dshydlouski <dshydlouski@scnsoft.com>
2024-01-05 17:24:26 +0300

    #9: Editing code for better code reading

--- ScnSoft-Education/LeetCodeTraining.Tests/PalindromeNumberSolitionTests.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/PalindromeNumberSolitionTests.cs
@@ -4,46 +4,14 @@ namespace LeetCodeTraining.Tests
 {
     public class PalindromeNumberSolitionTests
     {
-        [Fact]
-        public void PalindromeNumber_StringWith3Nums()
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        public void PalindromeNumberTests(int x, bool expectedResult)
         {
             //Arrange
-            int x = 121;
-            bool expectedResult = true;
-            bool result;
-
-            //Act
-            result = PalidnromeNumberSolution.IsPalindrome(x);
-
-            //Assert
-            Assert.Equal(expectedResult, result);
-        }
-
-        [Fact]
-        public void PalindromeNumber_StringWithNegativeNums()
-        {
-            //Arrange
-            int x = -121;
-            bool expectedResult = false;
-            bool result;
-
-            //Act
-            result = PalidnromeNumberSolution.IsPalindrome(x);
-
-            //Assert
-            Assert.Equal(expectedResult, result);
-        }
-
-        [Fact]
-        public void PalindromeNumber_StringWith2Nums()
-        {
-            //Arrange
-            int x = 10;
-            bool expectedResult = false;
-            bool result;
-
-            //Act
-            result = PalidnromeNumberSolution.IsPalindrome(x);
+            bool result = PalidnromeNumberSolution.IsPalindrome(x);
 
             //Assert
             Assert.Equal(expectedResult, result);
--- ScnSoft-Education/LeetCodeTraining/PalindromeNumber9/PalidnromeNumberSolution.cs
+++ ScnSoft-Education/LeetCodeTraining/PalindromeNumber9/PalidnromeNumberSolution.cs
@@ -1,12 +1,13 @@
-﻿
-namespace LeetCodeTraining.PalindromeNumber9;
+﻿namespace LeetCodeTraining.PalindromeNumber9;
 
+//https://leetcode.com/problems/palindrome-number/
 public static class PalidnromeNumberSolution
 {
     public static bool IsPalindrome(int x)
     {
         char[] reversed = x.ToString().ToCharArray();
         Array.Reverse(reversed);
+
         return x.ToString() == new string(reversed);
     }
 }
