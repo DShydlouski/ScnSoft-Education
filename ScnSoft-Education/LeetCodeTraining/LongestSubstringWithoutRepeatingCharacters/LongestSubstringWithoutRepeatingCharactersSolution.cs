89670aef5ed76136e177415b2c323f0302ad0de4
dshydlouski <dshydlouski@scnsoft.com>
2024-01-08 14:58:41 +0300

    #3: Solving a "Longest Substring Without Repeating Characters" problem

---
+++ ScnSoft-Education/LeetCodeTraining/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersSolution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeTraining.LongestSubstringWithoutRepeatingCharacters
+{
+    //https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    public static class LongestSubstringWithoutRepeatingCharactersSolution
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            string buff = "";
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var pos = buff.IndexOf(s[i]);
+                if (pos != -1)
+                {
+                    i = i - (buff.Length - pos);
+                    if (count < buff.Length)
+                    {
+                        count = buff.Length;
+                    }
+                    buff = "";
+                }
+                else
+                {
+                    buff += s[i];
+                }
+            }
+            if (count < buff.Length)
+            {
+                count = buff.Length;
+            }
+            return count;
+        }
+    }
+}
