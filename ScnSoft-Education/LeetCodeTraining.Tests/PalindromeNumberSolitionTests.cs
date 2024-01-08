8f153e8ae955a75126cea991e862f517f724f7bc
DShydlouski <155064794+DShydlouski@users.noreply.github.com>
2024-01-08 11:53:27 +0300

    #9: Solution to a Palindrome Number problem with unit tests (#2)
    * #9: Solution to a Palindrome Number problem with unit tests
    
    * #9: Editing code for better code reading

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/PalindromeNumberSolitionTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeTraining.PalindromeNumber9;
+
+namespace LeetCodeTraining.Tests
+{
+    public class PalindromeNumberSolitionTests
+    {
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        public void PalindromeNumberTests(int x, bool expectedResult)
+        {
+            //Arrange
+            bool result = PalidnromeNumberSolution.IsPalindrome(x);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/PalindromeNumber9/PalidnromeNumberSolution.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeTraining.PalindromeNumber9;
+
+//https://leetcode.com/problems/palindrome-number/
+public static class PalidnromeNumberSolution
+{
+    public static bool IsPalindrome(int x)
+    {
+        char[] reversed = x.ToString().ToCharArray();
+        Array.Reverse(reversed);
+
+        return x.ToString() == new string(reversed);
+    }
+}
