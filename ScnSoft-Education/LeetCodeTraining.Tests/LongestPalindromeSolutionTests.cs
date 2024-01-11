49eaf11588ddafd7a6cc1a1d12ad7e40030a58d8
dshydlouski <dshydlouski@scnsoft.com>
2024-01-11 13:04:58 +0300

    #5: Solving "Longest Palindromic Substring" problem

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestPalindromeSolutionTests.cs
@@ -0,0 +1,19 @@
+using LeetCodeTraining.LongestPalindromicSubstring5;
+namespace LeetCodeTraining.Tests
+{
+    public class LongestPalindromeSolutionTests
+    {
+        [Theory]
+        [InlineData("babad", "bab")]
+        [InlineData("cbbd", "bb")]
+        [InlineData("abbacfcd", "abba")]
+        public static void Test1(string s, string expected)
+        {
+            //Arrange
+            string result = LongestPalindromicSubstringSolution.LongestPalindrome(s);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/LongestPalindromicSubstring5/LongestPalindromicSubstringSolution.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeTraining.LongestPalindromicSubstring5;
+
+public static class LongestPalindromicSubstringSolution
+{
+    public static bool IsPalindrome(string x)
+    {
+        char[] reversed = x.ToString().ToCharArray();
+        Array.Reverse(reversed);
+        return x.ToString() == new string(reversed);
+    }
+    public static string LongestPalindrome(string s)
+    {
+        int end = s.Length;
+        string result = "";
+        string buff = "";
+        int counter = 0;
+
+        for (int i = s.Length; i > 0; i--)
+        {
+            buff = s.Substring(counter, i);
+
+            if (IsPalindrome(buff))
+            {
+                result = buff;
+                break;
+            }
+            else
+            {
+                if (counter + i == s.Length)
+                {
+                    counter = 0;
+                }
+                else
+                {
+                    counter++;
+                    ++i;
+                }
+            }
+        }
+
+        return result;
+    }
+}
