4157b5b7a46115367d711b816b2e6b3d06e96912
dshydlouski <dshydlouski@scnsoft.com>
2024-03-25 20:16:00 +0300

    #11 Solving Container with most water Problem

---
+++ ScnSoft-Education/LeetCodeTraining/ContainerWithMostWater/ContainerWithMostWaterSolution.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeTraining.ContainerWithMostWater
+{
+    public static class ContainerWithMostWaterSolution
+    {
+        public static int MaxArea(int[] height)
+        {
+            int max = 0;
+
+            if (height != null)
+            {
+                int x = 0;
+                int y = height.Length - 1;
+
+                while (x < y)
+                {
+                    int leftLine = height[x];
+                    int rightLine = height[y];
+
+                    if (leftLine < rightLine)
+                    {
+                        max = Math.Max(max, leftLine * (y - x));
+                        ++x;
+                    }
+                    else
+                    {
+                        max = Math.Max(max, rightLine * (y - x));
+                        --y;
+                    }
+
+                }
+
+            }
+
+            return max;
+        }
+    }
+}
